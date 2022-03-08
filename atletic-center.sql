ae734aa85efad8cf19f49b2c0844bb6b3f712974
WilliamRVazquez <wvazquez0@ucol.mx>
2022-03-08 11:12:16 -0600

    cambios en la tabla employees

--- atletic-center.sql
+++ atletic-center.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `employees` ADD `email` VARCHAR(255) NULL AFTER `pass`, ADD `phone` VARCHAR(255) NOT NULL AFTER `email`;
+
+UPDATE `employees` SET `email` = 'admin@admin.com', `phone` = '3141234567' WHERE `employees`.`idEmployees` = 1
