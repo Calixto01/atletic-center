385dd2643494064d084c72ddec41b1cf18c75f73
Carlos Alberto Calixto Engelberger <ccalixtoengelbergergamerpro@gmail.com>
2022-03-07 22:44:37 -0600

    nuevas cosas

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <header>
         <nav class="main-header navbar navbar-expand navbar-dark navbar-dark">
             <div class="container">
-              <a href="index3.html" class="navbar-brand">
+              <a href="index.php" class="navbar-brand">
                 <img src="resources/images/logo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                      style="opacity: .8; width: 40px;">
                 <span class="brand-text font-weight-light">Admin</span>
@@ -28,10 +28,10 @@
                   <a href="index.php" class="nav-link">Home</a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                  <a href="#" class="nav-link">Contact</a>
+                  <a href="contacto.php" class="nav-link">Contact</a>
                 </li>
                 <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown4" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <a class="nav-link dropdown-toggle" href="/" id="navbarDropdown4" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Help
                   </a>
                   <div class="dropdown-menu" aria-labelledby="navbarDropdown4">
@@ -141,7 +141,7 @@
           <!-- /.navbar -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <a href="visualizacion.php" class="brand-link">
               <img src="resources/images/logo.jpg" alt="Admin Logo" class="brand-image img-circle elevation-3"
                    style="opacity: .8">
               <span class="brand-text font-weight-light">Atletic Center</span>
